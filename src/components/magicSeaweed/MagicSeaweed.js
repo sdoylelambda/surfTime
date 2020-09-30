4c4a5167a83f8e895d1e69e77064601fb557822e
Sean Doyle <sdoylelambda@gmail.com>
2020-09-30 15:55:27 -0400

    Displays Magic Seaweed in div using iframe.

---
+++ src/components/magicSeaweed/MagicSeaweed.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MagicSeaweed = () => {
+  return (
+    <div>
+      <img route='https://magicseaweed.com/Melbourne-Beach-Surf-Report/351/'></img>
+      <div class='thumbnail-container'>
+        <div class='thumbnail'>
+          <iframe
+            src='https://magicseaweed.com/Melbourne-Beach-Surf-Report/351/'
+            frameborder='0'
+            height='500px'
+            width='500px'
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MagicSeaweed
